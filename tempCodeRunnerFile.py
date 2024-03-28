bdc71232ad204e389f5a2a64a88744dd2920ff6b
Farhaan Ahmad Siddiqui <141417304+Farhaan-786@users.noreply.github.com>
2024-03-28 16:00:01 +0530

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+hide_streamlit_style = """
+            
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+            
+#             """
+# st.markdown(hide_streamlit_style, unsafe_allow_html = True)
